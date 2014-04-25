55f01d6cfa40760fb3bea0174a251e8eceabe6bf
Pete Bachant <petebachant@gmail.com>
2014-04-25 10:12:32 -0400

    Modify pvload.py to load walls

--- pvload.py
+++ pvload.py
@@ -8,20 +8,16 @@ try:
 except ImportError:
     pvloaded = False
 
-try:
-    casename = sys.argv[1]
-except AttributeError:
-    casename = "rvat"
+for f in ["turbine", "walls"]:
+    wd = "rvat-log/" + f
+    folders = sorted(os.listdir(wd))
 
-wd = casename + "-log/" + casename
-folders = sorted(os.listdir(wd))
-
-for folder in folders:
-    files = os.listdir(wd + "/" + folder)
-    files = sorted([int((file.replace("step_", "").split(".")[0])) for file in files])
-    files = ["step_"+str(file)+".vtk" for file in files]
-    if pvloaded:
-        OpenDataFile([wd+"/"+folder+"/"+file for file in files])
-        RenameSource(folder)
-    else: print(files)
-    
+    for folder in folders:
+        files = os.listdir(wd + "/" + folder)
+        files = sorted([int((file.replace("step_", "").split(".")[0])) for file in files])
+        files = ["step_"+str(file)+".vtk" for file in files]
+        if pvloaded:
+            OpenDataFile([wd+"/"+folder+"/"+file for file in files])
+            RenameSource(folder)
+        else: print(files)
+        
